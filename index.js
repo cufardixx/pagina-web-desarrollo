0e7a60c678200a141c353c7b117bca973e745dee
FacuPicia <69948158+cufardixx@users.noreply.github.com>
2023-04-09 20:56:41 -0300

    Add files via upload
    update

---
+++ index.js
@@ -0,0 +1,7 @@
+const name = document.querySelector('name')
+const email = document.querySelector('email')
+const btn = document.querySelector('submit')
+
+btn.addEventListener('click', (e) =>{
+    alert('Please')
+})
